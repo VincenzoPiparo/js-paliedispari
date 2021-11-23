99de19b1237399997fad1b0e89e9accd1ba210ac
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-23 18:09:44 +0100

    END

--- ESERCIZIO 2/js/script.js
+++ ESERCIZIO 2/js/script.js
@@ -7,4 +7,54 @@
 // Dichiariamo chi ha vinto.
 
 
-// 1 Fare scegliere all'utente un numero da 1 a 5
+// 1 Fare scegliere all'utente pari o dispari
+// usare il prompt per il pari o dispari
+const userOddorEven = prompt ('Choose odd or even');
+
+
+// 2 Fare scegliere all'utente un numero da 1 a 5
+// usare il prompt per fare scegliere il numero
+let userNumber = 0;
+// creiamo un ciclo infinito fino a che l'utente scelga un numero stabilito tra 1 e 5
+while (userNumber < 1 || userNumber > 5) {
+    userNumber  = parseInt (prompt ('Choose a number from 1 to 5'));
+    console.log (userNumber);
+}
+
+// 3 generare il numero random per il pc usando una funzione
+// creiamo la funzione
+const numberPc = getRndInteger (1, 5)
+console.log (numberPc);
+
+// 4 sommiamo i 2 numeri (sia dell'utente sia del pc)
+let sumUserAndPc = userNumber + numberPc
+console.log(sumUserAndPc);
+
+
+// 5 usando una funzione stabiliamo se la somma dei 2 numeri scelti dall'utente e dal pc è pari o dispari
+let finalResult = totSum (sumUserAndPc)
+console.log(finalResult)
+
+
+// 6 infine dichiariamo il vincitore
+if (userOddorEven === finalResult ) {
+    alert ('You WIN')
+}else{
+    alert('You LOSE')
+}
+
+// 7 FUNCTION ZONE
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+ }
+
+ function totSum (somma) {
+     let result;
+     if( somma % 2 === 0) {
+         result = 'even'
+     } else {
+         result = 'odd'
+     }
+
+     return result;
+ }
