70c1474d6994fb6d91a5e4c86f8bac22b96dc147
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-23 15:32:59 +0100

    ESERCIZIO CORRETTO E INSERITA LA FUNZIONE

--- ESERCIZIO 1/js/script.js
+++ ESERCIZIO 1/js/script.js
@@ -2,20 +2,28 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// 1 Chiedere all’utente di inserire una parola
 const inWord = prompt('Enter a word ')
 
-// 2 scrivere la parola al contrario
+// verifichiamo se la parola sia palindroma 
+let wordIsPalindrome = verifyWordPalindrome (inWord)
 
-let reverseWord = ''
 
-for (i = (inWord.length -1); i >= 0; i-- ) {
-    reverseWord =  reverseWord + inWord [i] 
-    console.log(reverseWord)
-}
+// finito il cilco e le if traformare tutto in una funzione
+function verifyWordPalindrome(inWord) {
 
-// 3 verificare se le parole sono uguali 
-if (inWord === reverseWord) {
-    console.log('The word is palindrome')
-}else {
-    console.log('The word is not palindrome')
+    let reverseWord = ''
+    for (i = (inWord.length -1); i >= 0; i-- ) {
+        reverseWord =  reverseWord + inWord [i] 
+        console.log(reverseWord)
+    }
+    
+    // 3 verificare se le parole sono uguali 
+    let theWordIsPal
+    if (inWord === reverseWord) {
+        theWordIsPal = true
+    }else {
+        theWordIsPal = false
+    }
+
 }
